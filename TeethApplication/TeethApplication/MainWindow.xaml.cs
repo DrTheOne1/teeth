397e0ca1c1dbe889028dd101309e155643e30e2f
Martin Björkvall <martin@hubb.se>
2018-09-19 22:43:24 +0200

    Added windows for login, main, patient, booking

---
+++ TeethApplication/TeethApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeethApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonShowPatients_Click(object sender, RoutedEventArgs e)
+        {
+            Patient patient = new Patient();
+            patient.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonBookTime_Click(object sender, RoutedEventArgs e)
+        {
+            Booking booking = new Booking();
+            booking.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
