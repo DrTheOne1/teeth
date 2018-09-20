3840e6efccab605f51f29a2b34ad8c65ec49b8bd
knnv <karyn-vo@outlook.com>
2018-09-20 12:18:09 +0200

    patientlist..

--- TeethApplication/TeethApplication/Patient.xaml.cs
+++ TeethApplication/TeethApplication/Patient.xaml.cs
@@ -19,12 +19,60 @@ namespace TeethApplication
     /// </summary>
     public partial class Patient : Window
     {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string BirthDate { get; set; }
+            public string Address { get; set; }
+            public string PhoneNumber { get; set; }
+
+
+
+        }
+
         public Patient()
         {
             InitializeComponent();
+
+
+           List<Person> patient = new List<Person>();
+            patient.Add(new Person() {
+                FirstName = "Kyle",
+                LastName = "Me",
+                BirthDate = "881022-1100",
+                Address = "St.2, Mölndal",
+                PhoneNumber = "082-1928372"});
+
+            patient.Add(new Person()
+            {
+                FirstName = "Lily",
+                LastName = "Kent",
+                BirthDate = "440122-1340",
+                Address = "Pilgatan 2, Göteborg",
+                PhoneNumber = "032-1235372"
+            });
+
+            PatientList.ItemsSource = patient;
+            
+
+           
+     
         }
 
         private void ButtonReturnToMain_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+
+        }
+
+        private void PatientList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
